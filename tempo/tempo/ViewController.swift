0b7a0be770852c695c4e9b6c7c73a6d740049f01
lukseroa <74515005+lukseroa@users.noreply.github.com>
2022-10-21 12:56:39 -0300

    Add tempo folder

---
+++ tempo/tempo/ViewController.swift
@@ -0,0 +1,63 @@
+//
+//  ViewController.swift
+//  tempo
+//
+//  Created by MacBook Pro on 21/10/22.
+//
+
+import UIKit
+import Foundation
+
+enum HorasDoDia: String {
+    case manha = "Bom dia"
+    case tarde = "Boa tarde"
+    case noite = "Boa noite"
+    case madrugada = "Ainda está acordado?"
+    case error = "Sem mensagem"
+}
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var minhaLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let now = Date()
+
+        let calendar = Calendar.current
+        
+        let hour = calendar.component(.hour, from: now)
+        
+        let currentDate = getCurrentDate(hour: hour)
+        
+        minhaLabel.text = showMessageFromDate(mensagem: currentDate)
+        
+    }
+
+    func getCurrentDate(hour:Int) -> HorasDoDia {
+
+    if hour >= 6 && hour < 12 {
+        view.backgroundColor = .blue
+        return HorasDoDia.manha
+        
+    } else if hour >= 12 && hour < 18 {
+        view.backgroundColor = .green
+        return HorasDoDia.tarde
+        
+    } else if hour >= 18 && hour < 24 {
+        view.backgroundColor = .purple
+        return HorasDoDia.noite
+
+    } else if hour >= 0 && hour < 6 {
+        view.backgroundColor = .red
+        return HorasDoDia.madrugada
+        }
+        
+        return HorasDoDia.error
+    }
+
+    func showMessageFromDate(mensagem:HorasDoDia) -> String {
+        mensagem.rawValue
+        }
+}
